da39a9e02c9b5180e041f0a4fc60cc7d6f78f00f
[BetitoOrtizz] <[albertoortizlozanol@gmail.com]>
2022-08-30 14:39:21 -0500

    Inicio Hover

---
+++ PaginaWebOficialCRIC/Inicio Hover/index.js
@@ -0,0 +1,14 @@
+p1 =
+if (p1 === "si" && p2 === "no" && p3 === "si" && p4 === "no" && p5 === si){
+    console.log("Tu resultado es . . .")
+}else if ("Preguntas"){
+    console.log("Tu resultado es . . .")
+}else if ("Preguntas"){
+    console.log("Tu resultado es . . .")
+}else if ("Preguntas"){
+    console.log("Tu resultado es . . .")
+}else if ("Preguntas"){
+    console.log("Tu resultado es . . .")
+}else if ("Preguntas"){
+    
+}
